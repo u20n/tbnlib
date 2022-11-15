6b1833bca618d9fe0d336b04410c68c0301f4b62
apius <notyoudev@gmail.com>
2022-11-14 20:29:07 -0500

    All Sorts of Improvments

--- mtest.cpp
+++ mtest.cpp
@@ -1,31 +1,41 @@
 #include "tbn.hpp"
 #include <cassert>
-std::vector<int> t {1,2,3,4,5,6,7,8,9,0};
 
+using namespace tbn;
 int main(void) {
-  /** Convert Testing */
-  big ca = Convert::Big("12345");
-  int ia = 1; int ib = 2;
-  assert(Convert::Concat(ia, ib) == 12);
-  assert(Convert::String(ca) == "12345");
-  assert(t.back() != t.at(t.size()-2));
+  /** Construct */
+  big ca("12345");
+  assert(ca.string() == "12345");
+  std::cout << "Postive Construct OK\n";
+  
+  big cb("-12345");
+  assert(cb.string() == "-12345");
+  std::cout << "Negative Construct OK\n";
 
   /** Add */
-  big aa = Convert::Big("12345"); big ab = Convert::Big("12345");
-  assert(Convert::String(add(aa, ab)) == "24690");
- 
+  big aa("12345"); big ab("12345");
+  assert(add(aa, ab).string() == "24690");
+  std::cout << "Add OK\n";
+
   /** Subtract */
-  big sa = Convert::Big("12345"); big sb = Convert::Big("12345");
-  big sc = Convert::Big("9"); big sd = Convert::Big("12345");
-  assert(Convert::String(sub(sa, sb)) == "0");
-  assert(Convert::String(sub(sc, sd)) == "-12336");
-  
+  big sa("12345"); big sb("12345");
+  big sc("9"); big sd("12345");
+
+  assert(sub(sa, sb).string() == "0");
+  std::cout << "Positive Sub OK\n";
+
+  assert(sub(sc, sd).string() == "-12336");
+  std::cout << "Negative Sub OK\n";
+
   /** Mod */
-  big ma = Convert::Big("12345"); big mb = Convert::Big("2");
+  big ma("12345"); big mb("2");
   // assert(Convert::String(mod(ma, mb)) == "1");
-  
+  // std::cout << "Construct OK\n";
+
   /** Div */
-  big da = Convert::Big("12345"); big db = Convert::Big("12345");
+  big da("12345"); big db("12345");
   // assert(Convert::String(fdiv(da, db)) == "1");
+  // std::cout << "Construct OK\n";
+
   return 0;
 }
--- tbn.hpp
+++ tbn.hpp
@@ -1,207 +1,141 @@
 #include <iostream>
-#include <vector>
-#include <tuple>
+#include <deque>
+#include <array>
 #include <utility>
-/** 
- * bigs are always **stored** in the flipped format
- * e.g. 123,456 = big{6,5,4,3,2,1}
- * ~~~
- * for sanity, Convert is provided for the translation
- * */
-using big = std::vector<unsigned int>;
-using signed_big = std::tuple<bool, big>;
+#include <cmath>
+#include <string>
 
-namespace unpack {
-  bool& sign(signed_big p) {
-    return std::get<0>(p);
+namespace tbn { // == begin namespace ==
+struct big {
+  // states //
+  bool sign = true;
+  std::size_t point = 0;
+  std::deque<std::size_t> num; // bigs are always stored flipped; make sure to translate!
+  
+  // translation //
+  std::string string() {
+    std::string s = (this->sign) ? "" : "-";
+    for (int i=this->num.size()-1; -1 < i; i--) {
+      s += (this->num.at(i) + '0');
+    } return s;
   }
-  big& big(signed_big p) {
-    return std::get<1>(p);
+
+  big(std::string s) {
+    if (s.at(0) == '-') {
+      this->sign = false;
+      s.erase(0,1);
+    }
+    this->num.resize(s.length());
+    std::size_t bi = 0;
+    for (int i = s.length()-1; -1 < i; i--) {
+      this->num.at(bi) = (s[i] - '0'); bi++;
+    } 
   }
+  big(){}
 };
 
-
-// QoL might be the addition of a big construction
-
 void print(big n) {
-  for (auto e:n) {std::cout << e;}
+  for (const auto& e: n.num) {std::cout << e;}
   std::cout  << '\n';
 }
 
-namespace Convert {
-  int Concat(int _a, int _b) {
-    std::string a = std::to_string(_a);
-    std::string b = std::to_string(_b);
-    std::string p = (a+b);
-    return stoi(p);
-  }
-
-  std::string String(big x) {
-    std::string s;
-    for (int i=x.size()-1; i>-1; i--) {
-      s+=(x.at(i) + '0');
-    };
-    return s;
-  }
-  std::string String(signed_big x) {
-    std::string s;
-    if (unpack::sign(x)) {
-      s+='-';
-    };
-    big xb = unpack::big(x);
-    for (int i=xb.size()-1; i>-1; i--) {
-      s+=(xb.at(i) + '0');
-    };
-    return s;
-  }
-      
-  big Big(std::string s) {
-    big v;
-    v.resize(s.length());
-    v.reserve(s.length());
-
-    int mi = 0;
-    for (int i=s.length()-1; i>-1; i--) {
-      v.at(mi) = (s[i] - '0');
-      mi++;
-    }
-    return v;
-  }
-}
-
+/**
+ * This will break for negative _a or _b;
+ * This is intentional: use sub(tract)!
+ * */
 big add(big _a, big _b) {
   big r, a, b;
+  auto& an = a.num; auto& bn = b.num; auto& rn = r.num;
+  
   // initial compare
-  if (_b.size() > _a.size()) {
+  if (_a.num.size() < _b.num.size()) {
     a = _b; b = _a;
   } else {
     a = _a; b = _b;
   }
+
   // addition
-  for (int i=0; i<b.size(); i++) {
-    unsigned int s = (a.at(i) + b.at(i));
-    if (s>9) {
-      a.at(i+1)+=1;
+  for (int i=0; i<bn.size(); i++) {
+    unsigned int s = (an.at(i) + bn.at(i));
+    if (9<s) {
+      an.at(i+1)+=1;
       s = (s-10);
     }
-    r.push_back(s);
+    rn.push_back(s);
   }
   // fix leftover digits
-  if (r.size() < a.size()) {
-    int d = a.size() - (a.size()-r.size());
-    for (int i=d; i<a.size(); i++) {
-      r.push_back(a.at(i));
+  if (rn.size() < an.size()) {
+    int d = an.size() - (an.size()-rn.size());
+    for (int i=d; i<an.size(); i++) {
+      rn.push_back(an.at(i));
     } 
   }
   
   return r;
 }
 
-signed_big sub(big _a, big _b) {
-  big a, b, rv;
-  bool rb = false;
-  if (_b.size() > _a.size()) {
-    rb = true;
-    a = _b; b = _a;
-  } else {
-    a = _a; b = _b;
+big sub(big _a, big _b) {
+  big a, b, r;
+  auto& an = a.num; auto& bn = b.num; auto& rn = r.num;
+
+  if(_b.num.size() <= _a.num.size()) {
+    for (int i = _a.num.size()-1; -1 < i; i--) {
+      if (_a.num.at(i) < _b.num.at(i)) r.sign = 0;
+    }
+  } else r.sign = 0;
+  
+  switch (r.sign) {
+    case 0:
+      a = _b; b = _a;
+      break;
+    case 1:
+      a = _a; b = _b;
+      break;
   }
-  for (int i=0; i<b.size(); i++) {
-    int s = (a.at(i) - b.at(i));
+  
+  while(bn.size() < an.size()) {bn.push_back(0);}
+  
+  for (int i=0; i<bn.size(); i++) {
+    int s = (an.at(i) - bn.at(i));
     if (s < 0) {
-      a.at(i+1)--;
+      an.at(i+1)--;
       s += 10;
     }
-    rv.push_back(s);
+    rn.push_back(s);
   }
-  // fill in
-  if (rb) {
-    for (int i=b.size(); i < a.size(); i++) {
-      rv.push_back(a.at(i));
-    }
-  }
+
   // get rid of trailing zeros
-  for (int i=rv.size()-1; i>0; i--) {
-    if (rv.at(i) == 0) {rv.pop_back();}
-  }
-  return signed_big(rb, rv);
-}
-
-
-/** fourier */
-big fdiv(big x, big y) {
-  // fix odd pairing
-  // honestly this might be unnessecary
-  if (x.size()%2) {
-    x.push_back(0);
-  }
-  int an[(y.size()/2)+(y.size()%2)]; // a_n array w/o leading zero
-  int tp = (x.size()/2)-1; // total pairs n
-  int cn[tp]; // c_n array
-  int j = 0; // index for c_n
-  // translate to 2 digit pairs
-  for (int i=x.size()-1; i>-1; i-=2) {
-    int p = Convert::Concat(x.at(i), x.at(i-1));
-    std::cout << "p: " << p << '\n';
-    std::cout << x.at(i) << '|' << x.at(i-1) << '\n'; 
-    // forming c_n
-    int c = p*(tp*100);
-    cn[j] = c;
-    tp--; j++;
-  }
-  int r = cn[0];
-  std::vector<int> b_n;
-  bool bi_done = false;
-  int i = 0;
-  while (!bi_done) {
-    // check for done
-    if (b_n.size() == (sizeof(an) / sizeof(*an))) {bi_done = true;}
-    int i = (i == 0) ? i-- : i;
-    int ls = (r + cn[i+1]);
-    int rs = 0;
-    if (!b_n.empty()) {
-      int j = 2;
-      while(j != i) {
-        // v should be (i-j+1), but b_n's index starts at 0
-        rs -= (b_n.at(i-j) * an[j-1]);
-      }
-    }
-    b_n.push_back((ls-rs)/an[0]);
-    r = ((ls-rs)%an[0]);
-    i++;
-  }
-  // interpret b terms
-  for (auto b: b_n) {
-     
-  }
-}
-
-big multi(big a, big b) {
-  big r;
-  size_t len = (a.size()+b.size());
-  r.resize(len);
-  r.reserve(len);
-
-  for (int i=0; i<a.size(); i++) {
-    for (int j=0; j<b.size(); j++) {
-      r.at(i+j) += a.at(i)*b.at(j);
-      r.at(i+j+1) = r.at(i+j+1) + r.at(i+j)/10;
-      r.at(i+j) %= 10;
-    }
-  }
-  if (r.back() == 0) {r.pop_back();}
-
+  for (int i=rn.size()-1; i>0; i--) {
+    if (rn.at(i) == 0) {rn.pop_back();}
+  } 
   return r;
 }
 
-/** [possible] FEATURE
- * ~~~~
- * this could very easily take any two bigs
- */
-int mod2(big x) {
+/** 
+ * == fourier ==
+ * < quotient, remainder >
+ * 
+ *
+ * */
+std::array<big, 2> div(big c, big a) { 
+
+}
+
+
+/** x mod 2 */
+std::size_t mod2(big x) {
+  auto& xn = x.num;
   int rem = 0;
-  for (int i=x.size()-1; i>-1; i--) {
-    rem = (rem*10 + x.at(i)) % 2;
+  for (int i=xn.size()-1; i>-1; i--) {
+    rem = (rem*10 + xn.at(i)) % 2;
   }
+
   return rem;
 }
+
+/** a mod b */
+// [TODO]
+int mod(big a, big b) { 
+}
+
+} // == end namespace ==
